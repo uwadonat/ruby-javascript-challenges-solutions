9c09e21098a4ac06abcb5b1763a826afe22c0e16
uwadonat <uwadonat@gmail.com>
2021-12-04 16:37:13 +0300

    define function

--- Counting-element/counting-elements.rb
+++ Counting-element/counting-elements.rb
@@ -1,3 +1,15 @@
-arr = [2, 8, 5, 10, 9, 6]
+
 def counting_elements(arr)
+    count = 0
+    new_arr = arr.sort
+    (0..new_arr.length - 1).each do |i|
+        (i + 1..new_arr.legth -1).each do |j|
+            if new_arr[j] == new_arr[i] + 1
+                count = count +1
+            end
+        end
+    end
+    return count
 end
+
+puts counting_elements([2, 8, 5, 10, 9, 6])
