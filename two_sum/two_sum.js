f5592bcfa28dea5ec50988fa0e355df73f476ff0
uwadonat <uwadonat@gmail.com>
2021-12-05 21:27:12 +0300

    Add input array

--- two_sum/two_sum.js
+++ two_sum/two_sum.js
@@ -1,4 +1,5 @@
 var two_sum = function (nums, target) {
+  document.getElementById("array").innerHTML = "[" + nums + "]";
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       sum = nums[i] + nums[j];
