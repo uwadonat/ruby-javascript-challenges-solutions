7b45e60e44baa7be0cba8f7e805b41fbd3b02595
uwadonat <uwadonat@gmail.com>
2021-12-06 19:32:55 +0300

    Display javascript in index

--- Counting-element/counting-elements.js
+++ Counting-element/counting-elements.js
@@ -1,12 +1,13 @@
 var countingElements = function (array) {
+  document.getElementById("array").innerHTML = "[" + array + "]";
   let arraySorted = array.sort();
   let count = 0;
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < arraySorted.length; j++) {
-      if ((arraySorted[j] = arraySorted[i] + 1)) {
+      if (arraySorted[j] == arraySorted[i] + 1) {
         count++;
       }
     }
   }
-  console.log(count);
+  document.getElementById("answer").innerHTML = count;
 };
