38ce16b3b7548c81a2fa823426c12d75a5a5d03f
uwadonat <uwadonat@gmail.com>
2021-12-06 19:21:15 +0300

    Define a function

--- Counting-element/counting-elements.js
+++ Counting-element/counting-elements.js
@@ -1,5 +1,12 @@
-var countingElements = function(array) {
-    for (let i = 0; i < array.length; i ++) {
-       for  
+var countingElements = function (array) {
+  let arraySorted = array.sort();
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < arraySorted.length; j++) {
+      if ((arraySorted[j] = arraySorted[i] + 1)) {
+        count++;
+      }
     }
-}
+  }
+  console.log(count);
+};
