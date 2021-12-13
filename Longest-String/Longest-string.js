a201a631e2b23dad2686aedb00630059ae86f5e4
uwadonat <uwadonat@gmail.com>
2021-12-13 22:04:32 +0300

    Change string to array

--- Longest-String/Longest-string.js
+++ Longest-String/Longest-string.js
@@ -1,3 +1,3 @@
-const Longest = function(s) => {
+const Longest = function (s) {
   return s;
-}
+};
--- Longest-String/Longest-string.rb
+++ Longest-String/Longest-string.rb
@@ -1,3 +1,6 @@
 def length_of_longest_substring(s)
-    
+
+   puts s.split("")
 end
+
+length_of_longest_substring("Donat")
