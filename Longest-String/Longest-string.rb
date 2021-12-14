184e9f7466ec4eb07ea549982ac4c22429d40f86
uwadonat <uwadonat@gmail.com>
2021-12-14 21:48:55 +0300

    loop through an array

--- Longest-String/Longest-string.rb
+++ Longest-String/Longest-string.rb
@@ -1,8 +1,11 @@
 def length_of_longest_substring(s)
    array = s.split("")
+   checker =[];
    longest_length = 0
   (0..array.length - 1).each do i
-     
+     if !include(s[i]) 
+      checker.push(s[i])
+     end
   end
 end
 
