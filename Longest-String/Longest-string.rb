3f3a10ace446674c46975cf5dc7015781020ef18
uwadonat <uwadonat@gmail.com>
2021-12-15 23:16:04 +0300

    add conditio

--- Longest-String/Longest-string.rb
+++ Longest-String/Longest-string.rb
@@ -3,7 +3,7 @@ def length_of_longest_substring(s)
    checker =[];
    longest_length = 0
   (0..array.length - 1).each do i
-     if (!include(s[i])) 
+     if (!checker.include(s[i])) 
       checker.push(s[i])
       longest_length ++
      else 
