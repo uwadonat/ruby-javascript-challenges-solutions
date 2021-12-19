0c3b6bad904584b86afec0350b3a5f4e71055350
uwadonat <uwadonat@gmail.com>
2021-12-19 18:37:02 +0300

    exchange condition

--- Longest-String/Longest-string.rb
+++ Longest-String/Longest-string.rb
@@ -2,16 +2,19 @@ def length_of_longest_substring(s)
    array = s.split("")
    checker =[];
    longest_length = 0
-  (0..array.length - 1).each do i
-     if (!checker.include(s[i])) 
-      checker.push(s[i])
-      longest_length ++
+  (0..array.length - 1).each do |i|
+    answer = checker.include(s[i])
+     if answer 
+        checker =[];
+        longest_length = 0
+      
      else 
-      checker =[];
-      longest_length = 0
+        checker.push(s[i])
+        longest_length ++
      end
-return longest_length
+
   end
+  return longest_length
 end
 
 length_of_longest_substring("Donat")
