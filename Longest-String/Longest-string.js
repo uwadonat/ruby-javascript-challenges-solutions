9e2a0441e9cb1bd378f1951c49d0f7987bd0c23f
uwadonat <uwadonat@gmail.com>
2021-12-20 20:38:51 +0300

    loop through string array

--- Longest-String/Longest-string.js
+++ Longest-String/Longest-string.js
@@ -2,6 +2,8 @@ const Longest = (s) => {
   let checker = [];
   let length_array = [];
   let length_counter = 0;
+  let string = s.split("");
+  for (let i = 0; i < string.length; i++) {}
 
   console.log(s);
 };
