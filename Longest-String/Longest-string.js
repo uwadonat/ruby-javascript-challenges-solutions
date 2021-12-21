793365a76e1409169349c2d839e58c7f74910904
uwadonat <uwadonat@gmail.com>
2021-12-21 21:29:44 +0300

    Count number of length

--- Longest-String/Longest-string.js
+++ Longest-String/Longest-string.js
@@ -4,10 +4,15 @@ const Longest = (s) => {
   let length_counter = 0;
   let string = s.split("");
   for (let i = 0; i < string.length; i++) {
-    let include = checker.include?(string[i])
+    let include = checker.include?(string[i]);
     if (include) {
       checker = [];
       length_counter = 0
+    }
+    else {
+      checker.push(string[i]);
+      length_counter = length_counter + 1;
+      length_array.push(length_counter);
     }
   }
 
