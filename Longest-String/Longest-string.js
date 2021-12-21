80718b45aa0f2f873a0ce075fc06df98e29a1e02
uwadonat <uwadonat@gmail.com>
2021-12-21 21:23:53 +0300

    Add condition

--- Longest-String/Longest-string.js
+++ Longest-String/Longest-string.js
@@ -3,7 +3,13 @@ const Longest = (s) => {
   let length_array = [];
   let length_counter = 0;
   let string = s.split("");
-  for (let i = 0; i < string.length; i++) {}
+  for (let i = 0; i < string.length; i++) {
+    let include = checker.include?(string[i])
+    if (include) {
+      checker = [];
+      length_counter = 0
+    }
+  }
 
   console.log(s);
 };
